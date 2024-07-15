92102db685a2e7f425765fee6fd9cf21b75cf2a0
wodud2565 <wodud8455@naver.com>
2024-07-15 22:21:59 +0900

    2message

--- SRC백업/7.13/src/components/AsideLeft.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AsideLeft = () => {
-  return <div className="aside-left">Aside Left</div>;
-};
-
-export default AsideLeft;
--- SRC백업/7월13-2/src/components/AsideRight.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AsideRight = () => {
-  return <div className="aside-right">Aside Right</div>;
-};
-
-export default AsideRight;
