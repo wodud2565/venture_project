2cb7614803b6a966e06b07bd88b0fe37aee25e55
wodud2565 <wodud8455@naver.com>
2024-10-14 21:55:23 +0900

    햄버거 메뉴 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-// 네비게이션바 { 왼쪽에 홈버튼, 오른쪽에 3개( 로그인 로그아웃 찜목록 버튼)}
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     const header = document.querySelector(".header");
     const setMarginTop = () => {
@@ -17,43 +18,63 @@ const Header = () => {
     };
   }, []);
 
+  // 메뉴 토글 함수
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="header">
       <nav>
-        <ul className="nav-left">
-          <li>
-            <Link to="/">
-              <img src="/icons/home.png" alt="Home" className="icon" />
-            </Link>
-          </li>
-        </ul>
+        <div className="nav-left">
+          <button onClick={toggleMenu} className="menu-button">
+            {isMenuOpen ? <img src="/icons/close.png" alt="Close Menu" className="icon" /> : <img src="/icons/open.png" alt="Open Menu" className="icon" />}
+          </button>
+        </div>
         <div className="nav-center">
-          <h1>Finding Car</h1>
+          <button className="menu-button">
+            <Link to="/">
+              <h1>Finding Car</h1>
+            </Link>
+          </button>
         </div>
         <ul className="nav-right">
           <li>
-            <Link to="/login">
-              <img src="/icons/login.png" alt="Login" className="icon" />
-            </Link>
+            <button className="menu-button">
+              <Link to="/login">
+                <img src="/icons/login.png" alt="Login" className="icon" />
+              </Link>
+            </button>
           </li>
           <li>
-            <Link to="/login">
-              <img src="/icons/login.png" alt="Login" className="icon" />
-            </Link>
-          </li>
-          <li>
-            <Link to="/wishlist">
-              <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
-            </Link>
-          </li>
-          <li>
-            <Link to="/postlist">
-              <img src="/icons/wishlist.png" alt="Postlist" className="icon" />
-            </Link>
+            <button className="menu-button">
+              <Link to="/wishlist">
+                <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
+              </Link>
+            </button>
           </li>
         </ul>
       </nav>
+
+      {/* 메뉴 리스트 - 헤더 아래에 표시됨 */}
+      {isMenuOpen && (
+        <div className="menu-list-container">
+          <ul className="menu-list">
+            <li>
+              <Link to="/postlist" onClick={toggleMenu}>
+                게시판
+              </Link>
+            </li>
+            <li>
+              <Link to="/review" onClick={toggleMenu}>
+                이차어때
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Header;
