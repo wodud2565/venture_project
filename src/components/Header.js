60e7868a6868ec00264637df1f3b2458b828e660
wodud2565 <wodud8455@naver.com>
2024-10-14 21:57:15 +0900

    햄버거 메뉴 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { auth } from "../firebase";
@@ -6,6 +6,7 @@ import { auth } from "../firebase";
 const Header = () => {
   const { user, isAuthenticated, logout } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const header = document.querySelector(".header");
@@ -30,51 +31,81 @@ const Header = () => {
       console.error("로그아웃 실패:", error.message);
     }
   };
+  // 메뉴 토글 함수
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className="header">
       <nav>
-        <ul className="nav-left">
-          <li>
-            <Link to="/">
-              <img src="/icons/home.png" alt="Home" className="icon" />
-            </Link>
-          </li>
-        </ul>
+        <div className="nav-left">
+          <button onClick={toggleMenu} className="menu-button">
+            {isMenuOpen ? <img src="/icons/close.png" alt="Close Menu" className="icon" /> : <img src="/icons/open.png" alt="Open Menu" className="icon" />}
+          </button>
+        </div>
         <div className="nav-center">
-          <h1>Finding Car</h1>
+          <button className="menu-button">
+            <Link to="/">
+              <h1>Finding Car</h1>
+            </Link>
+          </button>
         </div>
         <ul className="nav-right">
           {isAuthenticated ? (
             <>
               <li>{user && <div className="name">{user.displayName || `${user.lastName}`}</div>}</li>
               <li>
-                <button onClick={handleLogout}>
+                <button className="menu-button" onClick={handleLogout}>
                   <img src="/icons/logout.png" alt="Logout" className="icon" />
                 </button>
               </li>
               <li>
-                <Link to="/wishlist">
-                  <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
-                </Link>
+                <button className="menu-button">
+                  <Link to="/wishlist">
+                    <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
+                  </Link>
+                </button>
               </li>
             </>
           ) : (
             <>
               <li>
-                <Link to="/login">
-                  <img src="/icons/login.png" alt="Login" className="icon" />
-                </Link>
+                <button className="menu-button">
+                  <Link to="/login">
+                    <img src="/icons/login.png" alt="Login" className="icon" />
+                  </Link>
+                </button>
               </li>
               <li>
-                <Link to="/wishlist">
-                  <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
-                </Link>
+                <button className="menu-button">
+                  <Link to="/wishlist">
+                    <img src="/icons/wishlist.png" alt="Wishlist" className="icon" />
+                  </Link>
+                </button>
               </li>
             </>
           )}
         </ul>
       </nav>
+
+      {/* 메뉴 리스트 - 헤더 아래에 표시됨 */}
+      {isMenuOpen && (
+        <div className="menu-list-container">
+          <ul className="menu-list">
+            <li>
+              <Link to="/postlist" onClick={toggleMenu}>
+                게시판
+              </Link>
+            </li>
+            <li>
+              <Link to="/review" onClick={toggleMenu}>
+                이차어때
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
